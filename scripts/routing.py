6cda32098dd69e2891cf3b32034a8319f5f729a1
rluria14 <ryanlur14@gmail.com>
2021-10-11 18:53:47 -0400

    updates to routing.py

--- scripts/routing.py
+++ scripts/routing.py
@@ -18,14 +18,38 @@ pool = get_conn_pool({
 })
 
 ## avoid polygon
-def route_nogo(nogo_layer, long_s, lat_s, long_t, lat_t, nogo_layer_query=""):
+def route_nogo(nogo_layers_list, long_s, lat_s, long_t, lat_t, nogo_layer_query_list=""):
     s_geom = 'POINT({} {})'.format(long_s, lat_s)
     t_geom = 'POINT({} {})'.format(long_t, lat_t)
 
-    #s_query = f"SELECT id FROM ways_vertices_pgr ORDER BY the_geom <-> ST_GeometryFromText('{s_geom}',4326) LIMIT 1"
+    # GET NOGO LAYER GEOMETRY AND CREATE nogo_query
+    nogo_dict = dict(zip(nogo_layers_list, nogo_layer_query_list))
+    nogo_query = "SELECT ST_GEOMFROMEWKT(ST_COLLECT(a.shape)) FROM (SELECT "
+    for key, value in nogo_dict.items():
+        geom_query = f"SELECT f_geometry_column, type FROM geometry_columns WHERE f_table_name = '{key}'"
+        geom_field = fetch_all(pool, geom_query)[0]
+        geom_type = geom_field['type']
+        geom_colname = geom_field['f_geometry_column']
+        if geom_type == "POINT":
+            layer = f'ST_FORCE2D(ST_BUFFER({geom_colname}, .0005)) as shape FROM {key}'
+        elif geom_type == "MULTILINESTRING":
+            layer = f'ST_FORCE2D(ST_BUFFER({geom_colname}, .0001)) as shape FROM {key}'
+        else:
+            layer = f'ST_FORCE2D({geom_colname}) as shape FROM {key}'
+        nogo_query += layer
+        nogo_query += f" WHERE {value}"
+        if key != nogo_layers_list[-1]:
+            nogo_query += " UNION SELECT "
+        else:
+            nogo_query += ") as a"
+
+    print("NOGO QUERY: ", nogo_query)
+
+    # GET STARTING POINT
+    # s_query = f"SELECT id FROM ways_vertices_pgr ORDER BY the_geom <-> ST_GeometryFromText('{s_geom}',4326) LIMIT 1"
     s_query = f'''
  	SELECT v.id FROM (SELECT UNNEST(array[w.source, w.target]) as id, w.the_geom FROM ways as w WHERE NOT
-	ST_INTERSECTS(w.the_geom, (SELECT ST_COLLECT(hgis_hydrology_polylines.shape, hgis_admin3.shape) from hgis_hydrology_polylines, 	hgis_admin3 WHERE hgis_admin3.adm3_name = 'Kitisuru' LIMIT 1))
+	ST_INTERSECTS(w.the_geom, ({nogo_query}))
  	ORDER BY w.the_geom <-> ST_GeometryFromText('{s_geom}',4326) LIMIT 1) as ways2
  	JOIN ways_vertices_pgr as v
  	ON (v.id = ways2.id)
@@ -33,10 +57,12 @@ def route_nogo(nogo_layer, long_s, lat_s, long_t, lat_t, nogo_layer_query=""):
     '''
     source = fetch_all(pool, s_query)[0]
     print(source['id'])
+
+    # GET ENDING POINT
     #t_query = f"SELECT id FROM ways_vertices_pgr ORDER BY the_geom <-> ST_GeometryFromText('{t_geom}',4326) LIMIT 1"
     t_query = f'''
  	SELECT v.id FROM (SELECT UNNEST(array[w.source, w.target]) as id, w.the_geom FROM ways as w WHERE NOT
-	ST_INTERSECTS(w.the_geom, (SELECT ST_COLLECT(hgis_hydrology_polylines.shape, hgis_admin3.shape) from hgis_hydrology_polylines, 	hgis_admin3 WHERE hgis_admin3.adm3_name = 'Kitisuru' LIMIT 1))
+	ST_INTERSECTS(w.the_geom, ({nogo_query}))
  	ORDER BY w.the_geom <-> ST_GeometryFromText('{t_geom}',4326) LIMIT 1) as ways2
  	JOIN ways_vertices_pgr as v
  	ON (v.id = ways2.id)
@@ -44,6 +70,8 @@ def route_nogo(nogo_layer, long_s, lat_s, long_t, lat_t, nogo_layer_query=""):
     '''
     target = fetch_all(pool, t_query)[0]
     print(target['id'])
+
+    # GET ROUTE
     route_query = f'''
     SELECT ST_UNION(b.the_geom) AS geojson, SUM(b.length_m) AS length
 FROM
@@ -52,7 +80,7 @@ FROM
 FROM
     pgr_nogo_astar(
         'SELECT gid AS id, source, target, cost, reverse_cost, x1, y1, x2, y2, the_geom AS geom FROM ways',
-        (SELECT string_agg(ST_COLLECT(hgis_hydrology_polylines.shape, hgis_admin3.shape),'') FROM {",".join(nogo_layer)} {nogo_layer_query}),
+        ({nogo_query}),
         {source['id']},
         {target['id']},
         TRUE,
@@ -65,9 +93,6 @@ ON (b.gid = s.edge)
 WHERE the_geom is not NULL'''
     route = fetch_all(pool, route_query)[0]
     return route['geojson'], route['length']
-
-#print(route_nogo('hgis_admin3', 36.79718, -1.24822, 36.730310, -1.216682, "WHERE adm3_name = 'Kitisuru'"))
-
 
 ## avoid points
 def route_nogo_withindistance(nogo_layer, long_s, lat_s, long_t, lat_t, nogo_layer_query=""):
@@ -102,6 +127,55 @@ WHERE the_geom is not NULL'''.format(nogo_layer, nogo_layer_query, source['id'],
     route = fetch_all(pool, route_query)[0]
     return route['geojson'], route['length']
 
-# print(route_nogo_withindistance('hgis_bridges_tunnels', 36.7749665,-1.2681415, 36.730310, -1.216682))
+## standard route with no obstacles
+def route_standard(long_s, lat_s, long_t, lat_t):
+    s_geom = 'POINT({} {})'.format(long_s, lat_s)
+    t_geom = 'POINT({} {})'.format(long_t, lat_t)
 
-print(route_nogo(['hgis_hydrology_polylines', 'hgis_admin3'], 36.756149,-1.225946, 36.699016,-1.209493, "WHERE hgis_hydrology_polylines.objectid = 230 OR hgis_hydrology_polylines.objectid = 446 OR hgis_admin3.adm3_name = 'Kitisuru'"))
+    # GET STARTING POINT
+    s_query = f'''
+     	SELECT v.id FROM (SELECT UNNEST(array[w.source, w.target]) as id, w.the_geom FROM ways as w
+     	ORDER BY w.the_geom <-> ST_GeometryFromText('{s_geom}',4326) LIMIT 1) as ways2
+     	JOIN ways_vertices_pgr as v
+     	ON (v.id = ways2.id)
+     	ORDER BY v.the_geom <-> ST_GeometryFromText('{s_geom}',4326) LIMIT 1
+        '''
+    source = fetch_all(pool, s_query)[0]
+    print(source['id'])
+
+    # GET ENDING POINT
+    # t_query = f"SELECT id FROM ways_vertices_pgr ORDER BY the_geom <-> ST_GeometryFromText('{t_geom}',4326) LIMIT 1"
+    t_query = f'''
+     	SELECT v.id FROM (SELECT UNNEST(array[w.source, w.target]) as id, w.the_geom FROM ways as w
+     	ORDER BY w.the_geom <-> ST_GeometryFromText('{t_geom}',4326) LIMIT 1) as ways2
+     	JOIN ways_vertices_pgr as v
+     	ON (v.id = ways2.id)
+     	ORDER BY v.the_geom <-> ST_GeometryFromText('{t_geom}',4326) LIMIT 1
+        '''
+    target = fetch_all(pool, t_query)[0]
+    print(target['id'])
+
+    route_query = '''
+    SELECT ST_UNION(b.the_geom) AS geojson, SUM(b.length_m) AS length
+FROM
+(SELECT
+*
+FROM
+    pgr_astar(
+        'SELECT gid AS id, source, target, cost, reverse_cost, x1, y1, x2, y2, the_geom AS geom FROM ways',
+        {},
+        {},
+        TRUE,
+        5,
+        1.0,
+        1.0
+    )) s
+LEFT JOIN ways b
+ON (b.gid = s.edge)
+WHERE the_geom is not NULL'''.format(source['id'], target['id'])
+    route = fetch_all(pool, route_query)[0]
+    return route['geojson'], route['length']
+
+#print(route_nogo_withindistance('hgis_bridges_tunnels', 36.7749665,-1.2681415, 36.730310, -1.216682))
+print(route_nogo(['hgis_admin3', 'hgis_bridges_tunnels'], 36.751293, -1.271346, 36.726833, -1.190509, ["hgis_admin3.adm3_name = 'Kitisuru'", "hgis_bridges_tunnels.objectid = 810 OR hgis_bridges_tunnels.objectid = 811"]))
+# print(route_standard(36.751293, -1.271346, 36.726833, -1.190509))
